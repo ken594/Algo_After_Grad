89026b3736c75cd1d8c2ee0f3f1fd59c709cf6c9
ken594 <weihui99525@gmail.com>
2023-09-18 17:26:47 -0700

    tabulation

--- DP_problems/fib(n).js
+++ DP_problems/fib(n).js
@@ -1,6 +1,20 @@
 // write a function fib(n) that takes in number as an argument.
 // The function should return the n-th number of the Fibonacci sequence.
 
+// Tabulation
+// Time: O(n), Space: O(n)
+const tabFib = (n) => {
+    const table = Array(n + 1).fill(0);
+    table[1] = 1;
+
+    for (let i = 0; i <= n; i++) {
+        table[i + 1] += table[i];
+        table[i + 2] += table[i];
+    }
+
+    return table[n];
+};
+
 // memoization
 //      js object, keys will be arg to fn, value will be the return value
 // Time: O(2n), Space: O(n)
@@ -21,7 +35,12 @@ const fib = (n) => {
     return fib(n - 1) + fib(n - 2);
 };
 
-console.log(memoFib(6));  // 8
-console.log(memoFib(7));  // 13
-console.log(memoFib(8));  // 21
-console.log(memoFib(50));  // 12586269025
+// console.log(memoFib(6));  // 8
+// console.log(memoFib(7));  // 13
+// console.log(memoFib(8));  // 21
+// console.log(memoFib(50));  // 12586269025
+
+console.log(tabFib(6));  // 8
+console.log(tabFib(7));  // 13
+console.log(tabFib(8));  // 21
+console.log(tabFib(50));  // 12586269025
